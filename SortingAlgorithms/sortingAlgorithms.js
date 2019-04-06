80d7b0fcb45b5114269bc8460507548c8ba85560
ChristianMarca <cmarcag@hotmail.com>
2019-04-05 20:51:03 -0500

    Update 05/04/2019

--- SortingAlgorithms/sortingAlgorithms.js
+++ SortingAlgorithms/sortingAlgorithms.js
@@ -1,9 +1,14 @@
-function interseccion (a,prim,ult){
+module.exports =function Sort(arr){
+    this.arr=arr;
+}
+
+Sort.prototype.interseccion=function (prim=0,ult=(this.arr.length-1)){
     //Complejidad 
     //mejor 14n-11
     //peor 9/2n^2+13/2n-10
     //media 9/2n^2+47/4n-11
     let i,j,x;
+    let a=this.arr;
     for(i=prim+1;i<=ult;i++){
         x=a[i]; j=i-1;
         while(j>=prim && x<a[j]){
@@ -15,7 +20,7 @@ function interseccion (a,prim,ult){
     return a
 };
 
-function posMax(a,i,j){
+Sort.prototype._posMax=function(a,i,j){
     let pmax,k;
     pmax=i;
     for(k=i+1;k<=j;k++){
@@ -26,7 +31,7 @@ function posMax(a,i,j){
     return pmax
 }
 
-function posMin(a,i,j){
+Sort.prototype._posMin=function(a,i,j){
     let pmin,k;
     pmin=i;
     for(k=i+1;k<=j;k++){
@@ -37,45 +42,45 @@ function posMin(a,i,j){
     return pmin
 }
 
-function intercambia(a,i,j){
+Sort.prototype._intercambia=function(a,i,j){
     let temp;
     temp=a[i];
     a[i]=a[j];
     a[j]=temp;
-
     return a
 }
 
-function seleccion(a,prim,ult){
+Sort.prototype.seleccion=function(prim,ult){
     //Complejidad 
     //mejor 3n^2+14n-14
     //peor 7/2n^2+27/2n-14
     //media 13/4n^2+55/4n-14
     let i;
+    let a=this.arr;
     for(i=prim;i<=ult-1;i++){
-        intercambia(a,i,posMin(a,i,ult))
+        this._intercambia(a,i,this._posMin(a,i,ult));
     }
     return a
 }
 
-function burbuja(a,prim,ult){
+Sort.prototype.burbuja=function(prim,ult){
     //Complejidad 
     //mejor 7/2n^2+5/2n-3
     //peor 8n^2-2n-1
     //media 23/4n^2+1/4n-1
     let i,j;
+    let a= this.arr;
     for(i=prim; i<=ult-1;i++){
         for(j=ult; j>=i+1;j--){
             if(a[j-1]>a[j]){
-                console.log(a)
-                intercambia(a,j-1,j);
+                this._intercambia(a,j-1,j);
             }
         }
     }
     return a
 }
 
-function combinarUno(a,p,q,r) {
+Sort.prototype._combinarUno=function(a,p,q,r) {
     let n1,n2;
     n1=q-p+1;
     n2=r-q;
@@ -105,7 +110,7 @@ function combinarUno(a,p,q,r) {
     }
     return a;
 }
-function combinar(a,b,p1,u1,p2,u2){
+Sort.prototype._combinar=function(a,b,p1,u1,p2,u2){
     let i1,i2;
     if((p1>u1)||(p2>u2)) return
     for(let k=p1;k<=u2;k++){
@@ -132,36 +137,41 @@ function combinar(a,b,p1,u1,p2,u2){
     return a
 }
 
-function mezcla(a,b,prim,ult){
+Sort.prototype.merge=function(prim,ult,type){
+    return type?
+        this._mezcla(this.arr,new Array(this.arr.length),prim,ult)
+        :
+        this._mezclaUno(this.arr,prim,ult);
+}
+Sort.prototype._mezcla=function(a,b,prim,ult){
     //Complejidad 
     //nlog(n)
     let mitad;
     if(prim<ult){
         mitad=Math.floor((prim+ult)/2);
-        mezcla(a,b,prim,mitad);
-        mezcla(a,b,mitad+1,ult);
-        combinar(a,b,prim,mitad,mitad+1,ult);
+        this._mezcla(a,b,prim,mitad);
+        this._mezcla(a,b,mitad+1,ult);
+        this._combinar(a,b,prim,mitad,mitad+1,ult);
     }
     return a
 }
-function mezclaUno(a,prim,ult){
+Sort.prototype._mezclaUno=function(a,prim,ult){
     //Complejidad 
     //nlog(n)
     let mitad;
     if(prim<ult){
         mitad=Math.floor((prim+ult)/2);
-        mezclaUno(a,prim,mitad);
-        mezclaUno(a,mitad+1,ult);
-        combinarUno(a,prim,mitad,ult);
+        this._mezclaUno(a,prim,mitad);
+        this._mezclaUno(a,mitad+1,ult);
+        this._combinarUno(a,prim,mitad,ult);
     }
     return a
 }
 
-function empujar(a,prim,ult,i){
+Sort.prototype._empujar=function(a,prim,ult,i){
     let j,k;
     k=i-prim+1;
     while(j!=k){
-        console.log('int..',a)
         j=k;
         if((2*j<=ult-prim+1) && (a[2*j+prim-1]>a[k+prim-1])){
             k=2*j;
@@ -169,30 +179,31 @@ function empujar(a,prim,ult,i){
         if((2*j<ult-prim+1) && (a[2*j+prim]>a[k+prim-1])){
             k=2*j+1;
         }
-        intercambia(a,j+prim-1,k+prim-1);
+        this._intercambia(a,j+prim-1,k+prim-1);
     }
     return a
 }
 
-function hacerMonticulo(a,prim,ult){
+Sort.prototype._hacerMonticulo=function(a,prim,ult){
     let i;
     for(i=Math.floor((ult-prim+1)/2);i>=1;i--){
-        empujar(a,prim,ult,prim+i-1);
+        this._empujar(a,prim,ult,prim+i-1);
     }
     return a
 }
 
-function monticulo(a,prim,ult){
+Sort.prototype.monticulo=function(prim,ult){
     let i;
-    hacerMonticulo(a,prim,ult);
+    let a=this.arr;
+    this._hacerMonticulo(a,prim,ult);
     for(i=ult;i>=(prim+1);i--){
-        intercambia(a,prim,i);
-        empujar(a,prim,i-1,prim);
+        this._intercambia(a,prim,i);
+        this._empujar(a,prim,i-1,prim);
     }
     return a
 }
 
-function privote(a,p,prim,ult){
+Sort.prototype._privote=function(a,p,prim,ult){
     let i,l;
     i=prim;
     l=ult;
@@ -203,7 +214,7 @@ function privote(a,p,prim,ult){
         l--;
     }
     while(i<l){
-        intercambia(a,i,l);
+        this._intercambia(a,i,l);
         while(!(a[i]>p)){
             i++;
         }
@@ -211,55 +222,29 @@ function privote(a,p,prim,ult){
             l--;
         }
     }
-    intercambia(a,prim,l);
+    this._intercambia(a,prim,l);
     return l
 }
-function quickSort(a,prim,ult){
+Sort.prototype.quickSort=function(prim,ult){
     //complejidad
     //peor O(n^2)
     //mejor y medio O(nlogn)
     let l;
+    let a=this.arr;
     if(prim<ult){
-        l=privote(a,a[prim],prim,ult);
-        quickSort(a,prim,l-1);
-        quickSort(a,l+1,ult);
+        l=this._privote(a,a[prim],prim,ult);
+        this.quickSort(a,prim,l-1);
+        this.quickSort(a,l+1,ult);
     }
     return a
 }
 
-// function shellSort(a,prim,ult){
-//     let j,h,N,v;
-//     N=(ult-prim+1);
-//     h=1;
-//     while(!(h>N)){
-//         h=3*h+1;
-//     }
-//     console.log(h,'sad')
-//     do{
-//         h=Math.floor(h/3);
-//         for(let i=h;i<N;i++){
-//             v=a[i+prim];
-//             j=i;
-//             while((j>=h)&&(a[j-h+prim]>v)){
-//                 a[j+prim]=a[j-h+prim];
-//                 console.log(j,h)
-//                 j=j-h;
-//                 console.log(j)
-//                 console.log(a,'nnn')
-//                 // h--;
-//             }
-//             a[j+prim]=v;
-//         }
-//         console.log(a,h,'++')
-//     }while(!(h=1))
-//     return a
-// }
-function shellSort(a,prim,ult){
+Sort.prototype.shellSort=function(prim,ult){
+    let a= this.arr;
     let N=ult-prim+1;
     var increment = (N/2);
     while (increment > 0) {
         for (let i = increment; i < N; i++) {
-            console.log('incr',i)
             var j = i;
             var temp = a[i+prim];
     
@@ -277,8 +262,17 @@ function shellSort(a,prim,ult){
             increment = parseInt(increment*5 / 11);
         }
     }
-  return a;
+  return a.slice(prim,ult+1);
 }
+// let sort=new Sort([2,45,2,5,6,3,1]);
+// console.table(sort.interseccion(0,6))
+// console.table(sort.seleccion(0,6));
+// console.table(sort.burbuja(0,6));
+// console.table(sort.merge(0,6,0));
+// console.table(sort.merge(0,6,1));
+// console.table(sort.monticulo(0,6));
+// console.table(sort.quickSort(0,6));
+// console.table(sort.shellSort(0,6))
 // console.table(interseccion([2,45,2,5,6,3,1],0,6))
 // console.log(posMax([2,45,2,5,6,3,1],0,6))
 // console.log(posMin([2,45,2,5,6,3,1],0,6))
@@ -288,5 +282,5 @@ function shellSort(a,prim,ult){
 // console.table(mezcla([2,45,2,5,6,3,1],new Array(7),0,6))
 // console.table(mezclaUno([2,45,2,5,6,3,1],0,6))
 // console.table(monticulo([2,45,2,5,6,3,1],0,6))
-console.table(quickSort([2,45,2,5,6,3,1],0,6))
-console.table(shellSort([2,45,2,5,6,3,1],0,6))
+// console.table(quickSort([2,45,2,5,6,3,1],0,6))
+// console.table(shellSort([2,45,2,5,6,3,1],0,6))
